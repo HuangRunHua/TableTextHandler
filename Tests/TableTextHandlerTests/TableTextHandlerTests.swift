657ca884f19059e4612fc54f30d2977833d0248d
Huang Runhua <45262463+HuangRunHua@users.noreply.github.com>
2022-05-25 18:59:39 +0800

    Update TableTextHandlerTests.swift

--- Tests/TableTextHandlerTests/TableTextHandlerTests.swift
+++ Tests/TableTextHandlerTests/TableTextHandlerTests.swift
@@ -6,6 +6,5 @@ final class TableTextHandlerTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(TableTextHandler().text, "Hello, World!")
     }
 }
